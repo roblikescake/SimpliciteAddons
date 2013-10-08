b2f1953de64d3b5744bfd741e5e73780f0f6d327
roblikescake <roblik3scak3@gmail.com>
2013-10-08 01:47:06 -0400

    Sure, why not.

--- src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/SimpliciteAddons.java
+++ src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/SimpliciteAddons.java
@@ -5,12 +5,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpliciteAddons extends JavaPlugin{
 
+    @Override
     public void onEnable() {
         PlayerListener playerListener = new PlayerListener(this);
         getServer().getPluginManager().registerEvents(playerListener, this);
         this.getServer().addRecipe(playerListener.getAnimalCatcherRecipe());
     }
 
+    @Override
     public void onDisable() {
         getServer().clearRecipes();
     }
