8797d10956acf6a37674bfec4d7f935a2921a2a6
roblikescake <roblik3scak3@gmail.com>
2013-10-08 01:15:16 -0400

    Merging All The Things!

---
+++ src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/SimpliciteAddons.java
@@ -0,0 +1,17 @@
+package net.simplicite_mc.roblikescake.simpliciteaddons;
+
+import net.simplicite_mc.roblikescake.simpliciteaddons.listeners.PlayerListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpliciteAddons extends JavaPlugin{
+
+    public void onEnable() {
+        PlayerListener playerListener = new PlayerListener(this);
+        getServer().getPluginManager().registerEvents(playerListener, this);
+        this.getServer().addRecipe(playerListener.getAnimalCatcherRecipe());
+    }
+
+    public void onDisable() {
+        getServer().clearRecipes();
+    }
+}
