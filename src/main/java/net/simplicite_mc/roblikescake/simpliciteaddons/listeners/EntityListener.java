a965557735c5cf58eac7cd62eefe5132c81deccf
TfT_02 <tft_02@hotmail.com>
2013-10-27 21:38:13 +0100

    Move head dropping into a seperate class

--- src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/listeners/EntityListener.java
+++ src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/listeners/EntityListener.java
@@ -1,14 +1,9 @@
 package net.simplicite_mc.roblikescake.simpliciteaddons.listeners;
 
-import java.util.Random;
-
-import org.bukkit.ChatColor;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,8 +13,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import net.simplicite_mc.roblikescake.simpliciteaddons.SimpliciteAddons;
-import net.simplicite_mc.roblikescake.simpliciteaddons.datatypes.HeadData;
-import net.simplicite_mc.roblikescake.simpliciteaddons.utilities.ItemManager;
+import net.simplicite_mc.roblikescake.simpliciteaddons.utilities.HeadManager;
 
 public class EntityListener implements Listener {
     public SimpliciteAddons pl;
@@ -60,37 +54,8 @@ public class EntityListener implements Listener {
         }
 
         Entity entity = event.getEntity();
-        EntityType entityType = event.getEntityType();
         Player killer = event.getEntity().getKiller();
 
-        HeadData headData = ItemManager.headData.get(entityType);
-
-        if (headData == null) {
-            return;
-        }
-
-        int lootBonus = 0;
-
-        if (killer.getItemInHand() != null) {
-            lootBonus = killer.getItemInHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS) * 2;
-        }
-
-        Location entityLocation = entity.getLocation();
-        Random random = new Random();
-        int diceRoll = random.nextInt(100);
-        String plPrefix = ChatColor.BLACK + "[" + ChatColor.AQUA + "SMC" + ChatColor.GRAY + "-" + ChatColor.DARK_AQUA + "Heads" + ChatColor.BLACK + "] ";
-
-        if (entity instanceof Player) {
-            if (diceRoll <= (10 + lootBonus)) {
-                String playerName = ((Player) entity).getName();
-                entityLocation.getWorld().dropItemNaturally(entityLocation, ItemManager.getPlayerHead(playerName));
-                pl.getServer().broadcastMessage(plPrefix + ChatColor.BLUE + playerName + ChatColor.GREEN + " was beheaded by " + ChatColor.BLUE + killer.getName() + ChatColor.GREEN  + "!");
-            }
-        } else {
-            if (diceRoll <= (ItemManager.headData.get(entityType).getDropChance() + lootBonus)) {
-                entityLocation.getWorld().dropItemNaturally(entityLocation, ItemManager.getMobHead(entityType));
-                killer.sendMessage(plPrefix + ChatColor.GREEN + "A " + ChatColor.BLUE + ItemManager.headData.get(entityType).getDisplayName() + ChatColor.GREEN + " dropped!");
-            }
-        }
+        HeadManager.dropHeads(entity, killer);
     }
 }
