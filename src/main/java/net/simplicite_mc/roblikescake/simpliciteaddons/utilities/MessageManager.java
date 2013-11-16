9b600ac1b8b34284860fcfa0bc7ef28cedce0069
roblikescake <roblik3cak3@gmail.com>
2013-11-16 17:02:55 -0500

    Update MOTD

--- src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/utilities/MessageManager.java
+++ src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/utilities/MessageManager.java
@@ -63,8 +63,8 @@ public class MessageManager {
 		String line2 = ChatColor.DARK_AQUA + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + "                 Simplicite" + ChatColor.BLUE + "" + ChatColor.BOLD + "MC\n";
 		String line3 = ChatColor.AQUA + "                    Barebones " + ChatColor.GOLD + getPluginVersionNumber() + "\n\n";
 		String line4 = ChatColor.DARK_GREEN + "                     Latest Changes:\n";
-		String line5 = ChatColor.YELLOW + "- Can now login via simplicite-mc.net\n";
-		String line6 = ChatColor.YELLOW + "- Added /motd : Displays PlayerMOTD\n";
+		String line5 = ChatColor.YELLOW + "- Sponges now remove water!\n";
+		String line6 = ChatColor.YELLOW + "- Can now login via simplicite-mc.net\n";
 		String line7 = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-";
 		return line1 + line2 + line3 + line4 + line5 + line6 + line7;
 	}
