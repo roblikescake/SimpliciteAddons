13d82c1a40cb87673316bc032c1d7faa77717972
roblikescake <roblik3scak3@gmail.com>
2014-02-01 21:31:14 -0500

    wut

--- src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/SimpliciteAddons.java
+++ src/main/java/net/simplicite_mc/roblikescake/simpliciteaddons/SimpliciteAddons.java
@@ -2,8 +2,6 @@ package net.simplicite_mc.roblikescake.simpliciteaddons;
 
 import net.simplicite_mc.roblikescake.simpliciteaddons.api.PlayerUUID.IdentifierAPI;
 import net.simplicite_mc.roblikescake.simpliciteaddons.commands.*;
-import net.simplicite_mc.roblikescake.simpliciteaddons.config.Config;
-import net.simplicite_mc.roblikescake.simpliciteaddons.database.MariaDB;
 import net.simplicite_mc.roblikescake.simpliciteaddons.listeners.BlockListener;
 import net.simplicite_mc.roblikescake.simpliciteaddons.listeners.EntityListener;
 import net.simplicite_mc.roblikescake.simpliciteaddons.listeners.PlayerListener;
@@ -14,14 +12,13 @@ import net.simplicite_mc.roblikescake.simpliciteaddons.utilities.ItemManager;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.Connection;
-
 public class SimpliciteAddons extends JavaPlugin {
 	public static SimpliciteAddons p;
 	public static IdentifierAPI identifier = new IdentifierAPI();
 
-	MariaDB mariaDB = new MariaDB(Config.getDBUser, Config.getDBPassword, Config.getDBHost, Config.getDBPort, Config.getDBDatabase);
-	Connection con = null;
+	public SimpliciteAddons() {
+		p = this;
+	}
 
 	@Override
 	public void onEnable() {
@@ -30,10 +27,6 @@ public class SimpliciteAddons extends JavaPlugin {
 		registerListeners();
 		registerManagers();
 		registerCommands();
-
-		loadDBConfig();
-
-		openDBConnection();
 	}
 
 	@Override
@@ -67,12 +60,4 @@ public class SimpliciteAddons extends JavaPlugin {
 		getCommand("getsponge").setExecutor(new CommandGetSponge());
 	}
 
-	public void openDBConnection() {
-		con = mariaDB.openConnection();
-	}
-
-	public void loadDBConfig() {
-		Config config = new Config(this);
-		config.loadConfigOptions();
-	}
 }
